fba8f5d278868749f9bd4b2f1765015a3567b183
MameDiarraDABO <90385644+MameDiarraDABO@users.noreply.github.com>
2022-05-25 17:39:09 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -84,7 +84,8 @@ if id_input not in liste_id:
     st.write("L'identifiant client n'est pas bon")
 
 
-elif (int(id_input) in liste_id):
+elif:
+    #(int(id_input) in liste_id)
     response = urlopen("https://app-api-2022.herokuapp.com/prediction/client/" + str(id_input))
     data_json = json.loads(response.read())
     hello = str(data_json["hello"])
